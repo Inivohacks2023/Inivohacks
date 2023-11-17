e8800e8e1d76d3030391d1bf6de5f39b8eab901e
SurainInivos <103168505+SurainInivos@users.noreply.github.com>
2023-11-18 00:25:10 +0530

    View history complete, Transferm Accept  rebrand,complete
    * vasic setip
    
    * commu
    
    * ccc
    
    * TRacker changes

--- Inivohacks.BL/DTOs/Models/RecallDTO.cs
+++ Inivohacks.BL/DTOs/Models/RecallDTO.cs
@@ -6,20 +6,14 @@ using System.Threading.Tasks;
 
 namespace Inivohacks.BL.DTOs.Models
 {
-    public class RebrandTrackerCodeDTO
+    public class RecallDTO
     {
-        public Guid CurrentTrackingCodeID { get; set; }
-      
-        public int ProductId { get; set; }
+        public Guid TrackingCode { get; set; }
+        public int BatchId { get; set; }
         public int CertificateId { get; set; }
-        public string Code { get; set; }
-        public int BatchNumber { get; set; }
-
         public string Latitude { get; set; }
         public string Longitude { get; set; }
         public string LocationName { get; set; }
         public int UserId { get; set; }
-
-
     }
 }
--- Inivohacks.BL/DTOs/Models/TransferBatchDTO.cs
+++ Inivohacks.BL/DTOs/Models/TransferBatchDTO.cs
@@ -9,10 +9,10 @@ namespace Inivohacks.BL.DTOs.Models
     public class TransferBatchDTO
     {
         public Guid TrackingCode { get; set; }
-        public int NewOwner { get; set; }
         public int CertificateId { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
         public string LocationName { get; set; }
+        public int UserId { get; set; }
     }
 }
