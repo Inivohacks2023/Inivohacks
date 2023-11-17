6d9aa08d090f4c9036deb18d31012741de89887d
azam <azam.fahmy@inivosglobal.com>
2023-11-17 20:14:12 +0530

    Added Permissions API

---
+++ Inivohacks.DAL/Repositories/PermissionRepository.cs
@@ -0,0 +1,37 @@
+﻿using Inivohacks.DAL.DataContext;
+using Inivohacks.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inivohacks.DAL.Repositories
+{
+    public class PermissionRepository: RepositoryBase<Permission>, IPermissionRepository
+    {
+        private readonly DatabaseContext _dbContext;
+        public PermissionRepository(DatabaseContext dbContext) : base(dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<bool> AddPermissionAsync(Permission permission)
+        {
+            await AddAsync(permission);
+            return true;
+        }
+
+        public async Task<Permission> GetPermissionbyPermissionIdAsync(int id)
+        {
+            return Search(a => a.PermissionId == id).FirstOrDefault();
+        }
+        public async IAsyncEnumerable<Permission> GetAllPermissionAsync()
+        {
+            foreach (var plist in _dbContext.Permissions.ToList())
+            {
+                yield return plist;
+            }
+        }
+    }
+}
---
+++ Inivohacks/Controllers/PermissionControlller.cs
@@ -0,0 +1,66 @@
+﻿using Inivohacks.BL.BLServices;
+using Inivohacks.BL.DTOs;
+using Inivohacks.Mapper;
+using Inivohacks.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inivohacks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : Controller
+    {
+        private readonly IPermissionService _permissionService;
+
+        public PermissionController(IPermissionService permissionService)
+        {
+            _permissionService = permissionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreation(PermissionModel viewModel)
+        {
+            bool status = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            status = await _permissionService.CreatePermissionAsync(
+                MapperExtentions.ToDto<PermissionModel, PermissionDto>(viewModel));
+
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PermissionModel>> PermissionDetailsbyID(int id)
+        {
+            var permission = await _permissionService.GetPermissionByIdAsync(id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(MapperExtentions.ToViewModel<PermissionDto, PermissionModel>(permission));
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<PermissionModel>> Get()
+        {
+
+            List<PermissionModel> results = new List<PermissionModel>();
+
+            IAsyncEnumerable<PermissionDto> permissionList = _permissionService.GetAllPermissionsAsync();
+            if (permissionList == null)
+            {
+                return NotFound();
+            }
+            await foreach (var r in permissionList)
+            {
+                results.Add(MapperExtentions.ToViewModel<PermissionDto, PermissionModel>(r));
+            }
+
+            return Ok(results);
+
+        }
+    }
+}
