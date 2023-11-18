4c9a5ad05b0a303839e2545e223452369fcbceb7
Tharindu Kanchana <Tharindu.Kanchana@inivossl.com>
2023-11-18 16:03:33 +0530

    Update manufacturer-home.component.ts

--- Inivohacks/ClientApp/src/app/manufacturer-home/manufacturer-home.component.ts
+++ Inivohacks/ClientApp/src/app/manufacturer-home/manufacturer-home.component.ts
@@ -12,11 +12,11 @@ export class ManufacturerHomeComponent {
 
   // Dummy data for the table
   medicines = [
-    { productId: 1, productName: 'Medicine A', dosage: '10mg', packageType: 'Box' },
-    { productId: 2, productName: 'Medicine B', dosage: '5mg', packageType: 'Bottle' },
-    { productId: 3, productName: 'Medicine C', dosage: '20mg', packageType: 'Blister' },
-    { productId: 4, productName: 'Medicine D', dosage: '15mg', packageType: 'Tube' },
-    { productId: 5, productName: 'Medicine E', dosage: '25mg', packageType: 'Packet' }
+    { productId: 1, productName: 'Amoxicillin', dosage: '10mg', packageType: 'Box' },
+    { productId: 2, productName: 'Metformin', dosage: '5mg', packageType: 'Bottle' },
+    { productId: 3, productName: 'Panadol', dosage: '20mg', packageType: 'Blister' },
+    { productId: 4, productName: 'Doxycycline', dosage: '15mg', packageType: 'Tube' },
+    { productId: 5, productName: 'Hydroxychloroquine', dosage: '25mg', packageType: 'Packet' }
   ];
 
   addCertificateLink = '/add-certificate';
